9784d257f78a6e26564c5638b95730eeee061cea
Guilherme <nightbane@gmail.com>
2021-04-21 23:00:53 +0200

    Update unity_license.sh

--- ci/unity_license.sh
+++ ci/unity_license.sh
@@ -6,5 +6,5 @@ set -x
 mkdir -p /root/.cache/unity3d
 mkdir -p /root/.local/share/unity3d/Unity/
 set +x
-echo 'Writing $UNITY_LICENSE_CONTENT to license file /root/.local/share/unity3d/Unity/Unity_lic.ulf'
-echo "$UNITY_LICENSE_CONTENT" | base64 --decode | tr -d '\r' > /root/.local/share/unity3d/Unity/Unity_lic.ulf
+echo 'Writing $UNITY_LICENSE_WEBGL to license file /root/.local/share/unity3d/Unity/Unity_lic.ulf'
+echo "$UNITY_LICENSE_WEBGL" | base64 --decode | tr -d '\r' > /root/.local/share/unity3d/Unity/Unity_lic.ulf
